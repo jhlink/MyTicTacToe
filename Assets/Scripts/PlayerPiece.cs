9ae25b1f8b7f674dfd1f3a23a0e65037e7dcf5e4
James <jhlink@users.noreply.github.com>
2018-02-18 16:36:08 -0500

    feat: Implement player piece move feature that guarantees centered placement

--- Assets/Scripts/PlayerPiece.cs
+++ Assets/Scripts/PlayerPiece.cs
@@ -17,10 +17,15 @@ public class PlayerPiece : MonoBehaviour {
 	public float gravityFactor = 10.0f;
 
 	private Vector3 forceDirection;
-	private bool isPlayerTurn = false;
 	private Rigidbody playerPieceRigidObject;
 	private BoxCollider playerPieceBoxCollider;
 	private GameLogic mainGameLogicController;
+	private Vector3 gridPlatePosition;	
+	private GameObject hitColliderGameObject;
+
+	private const float playerPieceDropHeight = .1f;
+	private const float playerPieceMoveSpeed = .5f;
+
 
     // Use this for initialization
     void Start() {
@@ -46,14 +51,13 @@ public class PlayerPiece : MonoBehaviour {
 		}
     }
 
-
 	void FixedUpdate() {
 		if (holdingPiece == true) {
 			Vector3 forwardDir = raycastHolder.transform.TransformDirection(Vector3.forward) * 100;
 			Debug.DrawRay(raycastHolder.transform.position, forwardDir, Color.green);
 
-
 			if (Physics.Raycast(raycastHolder.transform.position, (forwardDir), out hit)) {
+				hitColliderGameObject = hit.collider.gameObject;
 				gravityAttractor.transform.position = new Vector3(hit.point.x, hit.point.y + hoverHeight, hit.point.z);
 
 				playerPieceRigidObject.useGravity = false;
@@ -61,25 +65,55 @@ public class PlayerPiece : MonoBehaviour {
 
 				playerPieceRigidObject.AddForce(gravityAttractor.transform.position - this.gameObject.transform.position);
 
-				if (hit.collider.gameObject.tag == "Grid Plate") {
+				if (hitColliderGameObject.tag == "Grid Plate") {
 					if (Input.GetMouseButtonDown(0)) {
-						holdingPiece = false;
-						hit.collider.gameObject.SetActive(false);
-						hasBeenPlayed = true;
-						playerPieceRigidObject.useGravity = true;
-						playerPieceBoxCollider.enabled = true;
-						mainGameLogicController.playerMove(hit.collider.gameObject);
+						gridPlatePosition = hitColliderGameObject.transform.position;
+						hitColliderGameObject.SetActive(false);
+						playPiece ();
 					}
 				}
 			}
 		}
 	}
 
-    public void playPiece() {
-        //If the player has selected a grid area
-        //Animate the piece into position
-        hasBeenPlayed = true;
+	public void Move(Vector3 targetPlatePosition) {
+		Vector3 transformedVector = new Vector3 (targetPlatePosition.x, targetPlatePosition.y + playerPieceDropHeight, targetPlatePosition.z);
+		iTween.MoveTo (this.gameObject,
+			iTween.Hash(
+				"position", transformedVector,
+				"speed", playerPieceMoveSpeed,
+				"easetype", iTween.EaseType.easeInOutCubic,
+				"onstart", "prepareMove",
+				"oncomplete", "finishMove"
+			)
+		);
+	}
 
-        //Tell our GameLogic script to occupy the game board array at the right location with a player piece
+	private void prepareMove() {
+		playerPieceBoxCollider.enabled = true;
+		playerPieceRigidObject.useGravity = false;	
+		playerPieceRigidObject.isKinematic = true;
+	}
+
+	private void finishMove() {
+		playerPieceRigidObject.isKinematic = false;
+		playerPieceRigidObject.useGravity = true;	
+		gridPlatePosition = Vector3.zero;		
+		notifyGameLogicScriptOfNewPlacedPlayerPiece ();
+	}
+
+	public void playPiece() {
+		//If the player has selected a grid area				
+		holdingPiece = false;
+		hasBeenPlayed = true;
+
+        //Animate the piece into position 
+		Move(gridPlatePosition);	
     }
+
+	private void notifyGameLogicScriptOfNewPlacedPlayerPiece() {
+		//Tell our GameLogic script to occupy the game board array at the right location with a player piece
+		mainGameLogicController.playerMove(hitColliderGameObject);
+		hitColliderGameObject = null;
+	}
 }
